29bde485f25d8031a2726618db0b09ad9ad4c8fc
Wangwei <1816340588@qq.com>
2021-07-01 10:54:46 +0800

    修改FirstClass

--- test-git-project/src/main/java/com/atguigu/git/FirstClass.java
+++ test-git-project/src/main/java/com/atguigu/git/FirstClass.java
@@ -1,6 +1,7 @@
 package com.atguigu.git;
 
 public class FirstClass {
+
     public static void main(String[] args) {
         System.out.println("hello git");
     }
@@ -11,6 +12,13 @@ public class FirstClass {
 
     public static void solve(int a){
         System.out.println(a);
+        //新增代码
+        int b = 10;
+        System.out.println(b);
+    }
+
+    public boolean valid(){
+        return false;
     }
 
 }
