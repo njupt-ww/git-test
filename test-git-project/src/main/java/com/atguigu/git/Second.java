5e068c8feaf48ea8a73ad6cfebb4659577e799be
Wangwei <1816340588@qq.com>
2021-07-01 11:02:34 +0800

    添加一个Second类

---
+++ test-git-project/src/main/java/com/atguigu/git/Second.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public void test(){
+        System.out.println("dev branch");
+    }
+}
